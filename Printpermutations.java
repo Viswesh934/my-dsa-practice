cc1ab30717e0cda6a27b1ea86bcdebdaf717a283
sigireddy viswesh <98519767+Viswesh934@users.noreply.github.com>
2023-02-20 22:54:16 +0530

    Update Printpermutations.java

--- Printpermutations.java
+++ Printpermutations.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import java.util.*;
 public class Printpermutations {
     public static void main(String args[]){
@@ -23,29 +23,4 @@ public class Printpermutations {
 
     } 
 }
-=======
-import java.util.*;
-public class Printpermutations {
-    public static void main(String args[]){
-        int []arr={1,2,3};
-        Printpermutation(arr,0,new boolean[arr.length],new ArrayList<Integer>());
 
-    }
-    public static void Printpermutation(int arr[],int pos,boolean [] selected,ArrayList<Integer> ans){
-        if(pos==arr.length){
-            System.out.println(ans);
-            return;
-        }
-        for(int i=0;i<arr.length;i++){
-            if(selected[i]==false){
-                selected[i]=true;
-                ans.add(arr[i]);
-                Printpermutation(arr,pos+1,selected,ans);
-                selected[i]=false;
-                ans.remove(ans.size()-1);
-            }
-        }
-
-    } 
-}
->>>>>>> 3f55b46a0b05e454647dc1d57aa0747b7684deea
