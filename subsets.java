09a63a04b5139b07791b014068b6776f06627da1
sigireddy viswesh <98519767+Viswesh934@users.noreply.github.com>
2023-02-20 22:55:15 +0530

    Update subsets.java

--- subsets.java
+++ subsets.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 import java.util.*;
 public class subsets {
@@ -20,26 +20,3 @@ public class subsets {
     }
     
 }
-=======
-
-import java.util.*;
-public class subsets {
-    public static void main(String args[]){
-        int arr[]={8,90,4,20,10,34};
-        int target=120;
-        int i=0;
-        tss(arr,i,target,new ArrayList<>());
-    }
-    public static void tss(int arr[],int i,int target,ArrayList<Integer> ans){
-        if(i==arr.length){
-            System.out.println(ans);
-            return;
-        }
-        ans.add(arr[i]);
-        tss(arr,i+1,target-arr[i],ans);
-        ans.remove(ans.size()-1);
-        tss(arr,i+1,target,ans);
-    }
-    
-}
->>>>>>> 3f55b46a0b05e454647dc1d57aa0747b7684deea
